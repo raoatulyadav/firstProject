5b87994099bdaeeaf0884568413fa424633f1e4e
raoatulyadav <atulyaadav99@gmail.com>
2023-02-04 23:31:26 +0530

    upadte  something in index.js file

---
+++ index.js
@@ -0,0 +1,34 @@
+// function printGrade(number){ 
+// if(number== 100)
+// console.log("a")
+// }
+// {
+//   else if(number>= 90 & number <100)
+// console.log("b")
+// }
+// {
+// else if (number>=80 & number<90)
+// console.log("c")
+// }
+// {
+// else(number<=80)
+// console.log("f")
+
+// }
+// printGrade(80)
+let a ="manishyadav"
+function printGrade(number){
+    if(number==100){
+        console.log("A")
+    }
+    else if (number>=90&&number<100){
+        console.log("B")
+    }
+    else if(number>=80&&number<90){
+        console.log("C")
+    }
+    else{
+        console.log("F")
+    }
+}
+printGrade(80)
